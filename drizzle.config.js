dce8db48c3d3c24ee21ca45ec9089733546a2e24
SwapniL1563 <choratswapnil15@gmail.com>
2025-01-19 00:04:48 +0530

    updated

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./utils/schema.tsx",
   dbCredentials:{
-    url:"postgresql://aiOutput_owner:nzKbN8j6rCcZ@ep-billowing-frog-a5jxta5e.us-east-2.aws.neon.tech/aiOutput?sslmode=require"
+    url:NEXT_PUBLIC_DRIZZLE_DB_URL,
   }
 });
